1332b874cf3b9f910ca589ac81c01afcbe0dbad3
Elizabeth Paul <elizapaul01@gmail.com>
2019-03-30 00:32:56 +0100

    Add Comments

--- my-app/src/DisplayTexts.js
+++ my-app/src/DisplayTexts.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 function DisplayTexts(props){
+    // add quote text and author to tweet url
     let tweetUrl = `https://twitter.com/intent/tweet?text= ${props.quotesData.quoteText} - ${props.quotesData.quoteAuthor}`
 
     return (
         <div className="main">
             <h1>Random Quote Machine</h1>
             <div className="quote-box">
+                {/* Display quote text */}
                 <p>{props.quotesData.quoteText}</p>
+                {/* Display quote author */}
                 <p className="author">...<i class="fas fa-pencil-alt"></i> {props.quotesData.quoteAuthor}</p>
                 <div className="new_share_b">
+                    {/* call the handleClick function when button is click */}
                     <button className="new-quote" onClick={props.handleClick}>New Quote</button>
                     <div>
                         <a href={tweetUrl} target="_blank" rel="noopener noreferrer">
