3876ccb009d0c6c093727e74b691f7ed1ca32649
Elizabeth Paul <elizapaul01@gmail.com>
2019-03-29 23:59:57 +0100

    add share tweet
    Included the share tweet button
    
    and everything is working as it should

--- my-app/src/DisplayTexts.js
+++ my-app/src/DisplayTexts.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 function DisplayTexts(props){
+    let tweetUrl = `https://twitter.com/intent/tweet?text= ${props.quotesData.quoteText} - ${props.quotesData.quoteAuthor}`
+
     return (
         <div className="main">
             <h1>Random Quote Machine</h1>
             <div className="quote-box">
                 <p>{props.quotesData.quoteText}</p>
-                <p className="author">{props.quotesData.quoteAuthor}</p>
+                <p className="author">...<i class="fas fa-pencil-alt"></i> {props.quotesData.quoteAuthor}</p>
                 <div className="new_share_b">
                     <button className="new-quote" onClick={props.handleClick}>New Quote</button>
+                    <div>
+                        <a href={tweetUrl} target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-twitter fa-3x"></i>
+                        </a>
+                    </div>
                 </div>
             </div>            
         </div>
