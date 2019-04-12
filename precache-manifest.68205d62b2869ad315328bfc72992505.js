75b7976596c5bd070807d313f028299600bca14a
Elizabeth Paul <elizapaul01@gmail.com>
2019-04-12 23:54:25 +0200

    Updates

---
+++ precache-manifest.68205d62b2869ad315328bfc72992505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "798c666b1ab0bf1cc8d1",
+    "url": "/RandomQuoteReact/static/css/main.80069f94.chunk.css"
+  },
+  {
+    "revision": "798c666b1ab0bf1cc8d1",
+    "url": "/RandomQuoteReact/static/js/main.db06131c.chunk.js"
+  },
+  {
+    "revision": "fd9c7b78df9b7f36afe4",
+    "url": "/RandomQuoteReact/static/js/runtime~main.ca5ab00f.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/RandomQuoteReact/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "ca7f319c55308eab9bcbd60866fb04cc",
+    "url": "/RandomQuoteReact/index.html"
+  }
+];
